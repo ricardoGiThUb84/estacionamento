302093623554f6bbde2c3e1d770a713a6baeb3b9
Vinicius Machado <vinicius.desenvol@gmail.com>
2022-12-09 22:07:50 -0300

    Primeiro Commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
-import javax.management.MalformedObjectNameException;
-import java.util.Scanner;
+
 
 
 public class Main {
-    public static void main(String[] args) throws MalformedObjectNameException {
+    public static void main(String[] args)  {
 
         Scanner scan = new Scanner(System.in);
 
