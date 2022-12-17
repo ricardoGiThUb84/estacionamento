45054a8a28432b3c353b7856bce8fc79667999a4
Aline Mizumukai <aline.mizumukai@mag-it.com.br>
2022-12-17 19:01:06 -0300

    Ajuste na impressão dos dados da saida

--- src/entidade/RegistroSaida.java
+++ src/entidade/RegistroSaida.java
@@ -4,6 +4,7 @@ import BancoDeDados.Repositorio;
 import Utilitarios.ManipulaDatas;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class RegistroSaida extends Registro {
 
@@ -28,14 +29,32 @@ public class RegistroSaida extends Registro {
 
         if(horasPlano == 0){
             this.valor = (5 * hora);
+            imprimirRegistro(dadosEntrada);
         } else if (horasPlano >= hora){
             repositorio.atualizarVeiculo(veiculo, veiculo.getTipoPlano(), (int) -hora);
             this.valor = 0;
+            imprimirRegistro(dadosEntrada, horasPlano, (int)hora);
         } else if (horasPlano < hora){
             int saldoHoras = (int)hora - horasPlano;
             repositorio.atualizarVeiculo(veiculo, veiculo.getTipoPlano(), -horasPlano);
             this.valor = (5 * saldoHoras);
+            imprimirRegistro(dadosEntrada, horasPlano, (int)hora, saldoHoras);
         }
+    }
+
+    public void imprimirRegistro(Registro dadosEntrada){
+        System.out.printf("Veículo - Placa: %s - Modelo: %s %n\tData de Entrada: %s %n\tData da Saída: %s %n\tValor: R$ %.2f %n",
+                getVeiculo().getPlaca(), getVeiculo().getModelo(), dadosEntrada.getDataRegistro().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")), getDataRegistro().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")), valor);
+    }
+
+    public void imprimirRegistro(Registro dadosEntrada, int horasPlano, int duracao){
+        System.out.printf("Veículo - Placa: %s - Modelo: %s %n\tData de Entrada: %s %n\tData da Saída: %s %n\tSaldo de horas inicial: %d %n \tHoras descontadas: %d %n \tSaldo de horas atualizado: %d %n",
+                getVeiculo().getPlaca(), getVeiculo().getModelo(), dadosEntrada.getDataRegistro().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")), getDataRegistro().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")), horasPlano, duracao, (horasPlano - duracao));
+    }
+
+    public void imprimirRegistro(Registro dadosEntrada, int horasPlano, int duracao, int saldoHoras){
+        System.out.printf("Veículo - Placa: %s - Modelo: %s %n\tData de Entrada: %s %n\tData da Saída: %s %n\tSaldo de horas inicial: %d %n \tHoras descontadas: %d %n \tSaldo de horas atualizado: %d %n\tValor: R$ %.2f -> Referente ao saldo de %d horas %n",
+                getVeiculo().getPlaca(), getVeiculo().getModelo(), dadosEntrada.getDataRegistro().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")), getDataRegistro().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")), horasPlano, (duracao - saldoHoras), 0, valor, saldoHoras);
     }
 
     @Override
